e7ca8ecd725859bb26a6a5c46df9bd5dd837857d
Moein <moeinati.79@gmail.com>
2024-04-25 16:29:55 +0330

    1.2

--- server.js
+++ server.js
@@ -1,45 +1,66 @@
-
 const express = require('express');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = 3000;
 
-const friends = [
-    { id: 0, firstName: 'Moein', lastName: 'Atfinding' },
-    { id: 1, firstName: 'Ali', lastName: 'Jace' },
-    { id: 2, firstName: 'George', lastName: 'Bliumng' }
+const shop = [
+    { id: 0, name: 'concept1' },
+    { id: 1, name: 'concept2' },
+    { id: 2, name: 'concept3' }
 ];
 
 app.use((req, res, next) => {
-    const start = Date.now();
+    const startTime = Date.now();
     next();
-    const reqTime = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${reqTime}ms`);
-})
+    const endTime = Date.now() - startTime;
+    console.log(`${req.method} ${req.url} ${endTime}ms`)
+});
 
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = req.params.friendId;
-    const item = friends[friendId];
-    if (item) {
-        res.json(item)
-    } else {
-        res.status(404).json({ error: "NOT FOUNNNDDddddd" });
-    }
+app.use(express.json());
+
+app.get('/shop/:shopId', (req, res) => {
+    const shopId = req.params.shopId;
+    const item = shop[shopId];
+    if (item) { res.json(item) }
+    else {
+        res.status(404).json({ error: " ERROR " })
+    };
+
 });
 
 app.get('/', (req, res) => {
     res.send("Helloo from express");
 });
 
-app.get('/friends', (req, res) => {
-    res.send(friends);
+app.get('/about', (req, res) => {
+    res.send("About us tab")
 });
 
-app.post('/friends',(req,res)=>{
-
+app.get('/contact', (req, res) => {
+    res.send(`Contact Us ${res}`)
 })
+
+app.get('/shop', (req, res) => {
+    res.send(`Shop Tab, ${shop}`)
+})
+
+app.post('/shop', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({
+            error: "Item not found!"
+        })
+    };
+    let newItem = {
+        name: req.body.name,
+        id: shop.length
+    }
+    shop.push(newItem);
+    res.json(newItem)
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
-});
+})
+
 
