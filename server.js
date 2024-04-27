ff0161156812c6a225f3a7e5c04494f3a54ac9d0
The AMS <moeinati.79@gmail.comgit commit git config --global user.email moeinati.79@gmail.com>
2024-04-27 22:40:21 +0330

    1.3

--- server.js
+++ server.js
@@ -58,6 +58,46 @@ app.post('/shop', (req, res) => {
     shop.push(newItem);
     res.json(newItem)
 })
+app.put('/shop/:shopId', (req, res) => {
+
+    const item = shop[parseInt(req.params.shopId)];
+
+    if (!item) {
+        return res.status(404).json({
+            error: "item Not Found..."
+        });
+    }
+    if (!req.body.name) {
+        return res.status(404).json({
+            error: "You item Was Empty..."
+        });
+    }
+
+    item.name = req.body.name;
+
+    res.send(item);
+
+
+});
+
+app.delete('/shop/:shopId', (req, res) => {
+
+    const item = shop.find(f => f.id === parseInt(req.params.itemId));
+
+    if (!item) res.status(404).json({
+        error: "item Not Found..."
+    });
+
+    const itemIndex = shop.indexOf(item);
+
+    shop.splice(itemIndex, 1);
+
+
+    res.send(friend);
+
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
